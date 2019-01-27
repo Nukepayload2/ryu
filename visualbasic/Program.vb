c77a779b0d7a27cc278448c394c27a51366f1ac9
Nukepayload2 <1939357182@qq.com>
2019-01-27 22:53:12 +0800

    Added comments in entry point

--- visualbasic/Program.vb
+++ visualbasic/Program.vb
@@ -1,5 +1,9 @@
 Module Program
     Sub Main(args As String())
+        ' Converted from Java version.
+        ' This sample is not ready for performance tests, because it uses BigInteger, which is slow.
+        ' BigInteger is preserved for better readability.
+        ' If you need better performance, see the CSharp version. 
         Dim f As Single = 0.330078125F
         Dim result As String = RyuFloat.floatToString(f, RoundingMode.ROUND_EVEN)
         Console.WriteLine(result & " " & f)
